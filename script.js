249e8dff3a16eb05330edb6f6ec51af9634694c5
ferruhcihan <ferruhcihan20@gmail.com>
2020-11-07 01:55:53 +0300

    script.js deleted

--- script.js
+++
@@ -1,8 +0,0 @@
-const entryText = document.getElementById("entryText").value;
-const entryLetter = document.getElementById("entryLetter").value;
-
-function myFunc(){
-    const index = entryText.indexOf(entryLetter);
-
-    document.getElementById("result").innerHTML="Letter Index: " + index;
-}
